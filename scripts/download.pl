2c9a50002decc13aef3be8328c274a850d4db7ab
Ivan Krylov <krylov.r00t@gmail.com>
2013-12-10 21:42:14 +0400

    scripts/download.pl: use wget for actual downloading
    to have a chance to restart an interrupted download

--- scripts/download.pl
+++ scripts/download.pl
@@ -5,7 +5,5 @@ use WWW::Mechanize;
 my $m = WWW::Mechanize->new(autocheck => 1);
 my ($url,$regex,$where) = @ARGV;
 $m->get($url);
-$m->add_header(Referer => undef); # sf.net sends HTML if referrer is present
-for ($m->get( $m->find_link(url_regex => qr/$regex/) || die("$regex: link not found\n"), ':content_file' => $where )) {
-	die $_->status_line unless $_->is_success;
-}
+exec("wget", "-O", $where, "-c", ($m->find_link(url_regex => qr/$regex/) || die("$regex: link not found\n"))->url_abs);
+die "wget: $!\n";
